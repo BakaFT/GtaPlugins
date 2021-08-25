05d85d337c28bb90e37c79521b14fe47e36736f3
BakaFT <1355564740@qq.com>
2021-08-26 01:53:15 +0800

    Create removeBlocks.cs

---
+++ VC/removeBlocks.cs
@@ -0,0 +1,16 @@
+{$CLEO .cs}
+0000:
+while true
+    wait 0  
+        //COMGATE1             StarFish island west gate(near Cherry popper)       
+        0108:$1779
+        //COMGATE2            StarFish island east gate
+        0108:$1780   
+        //NT_ROADBLOCKCI    InterGlobal Studio
+        0108:$1781
+        //#NT_ROADBLOCKGF     Golf
+        0108:$1782
+        //#WSH_ROADBLOCK     Washington Street (near Hospital)
+        0108:$1783  
+end
+0A93: terminate_this_custom_script
